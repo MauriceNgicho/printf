f839e22868a6d6d42417fabab02760e6ea20cd4b
MauriceNgicho <mrcngicho@gmail.com>
2024-07-31 11:53:53 -0700

    header file

--- main.h
+++ main.h
@@ -8,7 +8,7 @@ int _printf(const char *format, ...);
 int print_char(char c);
 int print_string(const char *str);
 int print_int(int n);
-int specifiers_format(const char **format, va_list args);
+int handle_format(const char **format, va_list args);
 
 #endif /* MAIN_H */
 
