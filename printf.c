f9f27aa42773702b077bc86051bac522f31852ac
Christopher <crnjihia@gmail.com>
2024-07-30 20:01:09 -0700

    Update the _printf Function

--- printf.c
+++ printf.c
@@ -1,85 +1,111 @@
 #include "main.h"
 
-
 /**
- * num_of_char - Prints a string to stdout
+ * print_string - Prints a string to stdout
  * @str: The string to be printed
  *
  * Return: The number of characters printed
  */
-int num_of_char(const char *str)
+int print_string(const char *str)
 {
-	int count = 0;
+    int count = 0;
 
-
-	while (*str)
-
-	{
-		count += num_of_char(*str);
-		str++;
-	}
-	return (count);
+    while (*str)
+    {
+        count += print_char(*str);
+        str++;
+    }
+    return (count);
 }
 
-
 /**
- * prnt_char - Writes a character to stdout.
- * @c: The character to write to stdout.
+ * print_char - Writes a character to stdout
+ * @c: The character to write to stdout
  *
- * Return: On success, the number of bytes written (1).
+ * Return: On success, the number of bytes written (1)
  */
-
-int prnt_char(char c)
+int print_char(char c)
 {
-	return (write(1, &c, 1));
+    return (write(1, &c, 1));
 }
 
+/**
+ * print_int - Prints an integer to stdout
+ * @n: The integer to print
+ *
+ * Return: The number of characters printed
+ */
+int print_int(int n)
+{
+    int count = 0;
+    unsigned int num;
+
+    if (n < 0)
+    {
+        count += print_char('-');
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
+
+    if (num / 10)
+    {
+        count += print_int(num / 10);
+    }
+    count += print_char((num % 10) + '0');
+
+    return (count);
+}
 
 /**
- * _printf - Produces output according to format used
+ * _printf - Produces output according to a format
  * @format: A character string composed of zero or more directives
  *
- * Return: The number of characters printed (excluding the null byte
- * used to end output to strings)
+ * Return: The number of characters printed (excluding the null byte used to end output to strings)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	int p;
+    va_list args;
+    int count = 0;
+    int i;
 
-	va_start(args, format);
+    va_start(args, format);
 
-	for (p = 0; format[p] != '\0'; p++)
-	{
-		if (format[p] == '%')
-		{
-			i++;
-			if (format[p] == 'c')
-			{
-				count += num_of_char(va_arg(args, int));
-			}
-			else if (format[p] == 's')
-			{
-				count += num_of_char(va_arg(args, char *));
-			}
-			else if (format[p] == '%')
-			{
-				count += num_of_char('%');
-			}
-			else
-			{
-				count += num_of_char('%');
-				count += num_of_char(format[p]);
-			}
-		}
-		else
-		{
-			count += num_of_char(format[p]);
-		}
-	}
+    for (i = 0; format && format[i] != '\0'; i++)
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == 'c')
+            {
+                count += print_char(va_arg(args, int));
+            }
+            else if (format[i] == 's')
+            {
+                count += print_string(va_arg(args, char *));
+            }
+            else if (format[i] == '%')
+            {
+                count += print_char('%');
+            }
+            else if (format[i] == 'd' || format[i] == 'i')
+            {
+                count += print_int(va_arg(args, int));
+            }
+            else
+            {
+                count += print_char('%');
+                count += print_char(format[i]);
+            }
+        }
+        else
+        {
+            count += print_char(format[i]);
+        }
+    }
 
-
-	va_end(args);
-	return (count);
+    va_end(args);
+    return (count);
 }
